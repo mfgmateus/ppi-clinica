52266826edb9a00ba63a278c6cb04521402a1675
Mateus Ferreira <mfgmateus1996@gmail.com>
2017-11-11 21:06:12 -0200

    Creating schedule

--- schedule.php
+++ schedule.php
@@ -7,6 +7,44 @@ $sqlSpecialities = "SELECT ID, NAME FROM SPECIALITIES ORDER BY NAME";
 $specialities = select($sqlSpecialities);
 
 $times = array(8, 9, 10, 11, 13, 14, 15, 16, 17);
+
+if (isset($_POST['submit'])) {
+
+    $conn = get_connection();
+
+    $sqlPatient = "INSERT INTO PATIENTS(NAME, PHONE)" .
+        "VALUES(?, ?)";
+
+    $sqlSchedule = "INSERT INTO SCHEDULE(PATIENT, DOCTOR, SCHEDULE_DATE, SCHEDULE_TIME) " .
+        "VALUES ((SELECT MAX(ID) FROM PATIENTS), ?, ?, ?)";
+
+    $patientStmt = $conn->prepare($sqlPatient) or die("Falha ao inserir Paciente" . $conn->error);
+    $scheduleStmt = $conn->prepare($sqlSchedule) or die("Falha ao inserir Agenda" . $conn->error);
+
+    $patientStmt->bind_param("ss", $patient, $patient_phone);
+    $scheduleStmt->bind_param("isi", $doctor, $date, $hour);
+
+    $patient = $_POST["patient"];
+    $patient_phone = $_POST["patient-phone"];
+    $doctor = $_POST["doctor"];
+    $date = $_POST["date"];
+    $hour = $_POST["hour"];
+
+    $conn->begin_transaction();
+
+    $patientStmt->execute() or die("Falha ao agendar consulta (paciente)" . $conn->error);
+    $scheduleStmt->execute() or die("Falha ao agendar consulta (schedule)" . $conn->error);
+
+    $conn->commit() or die("Falha ao cadastrar  funcionario");
+
+    $patientStmt->close();
+    $scheduleStmt->close();
+
+    $conn->close();
+
+    echo "Consulta agendada!";
+}
+
 ?>
 <script>
 
@@ -17,7 +55,7 @@ $times = array(8, 9, 10, 11, 13, 14, 15, 16, 17);
     }
   }
 
-  function addOption(element, id, text){
+  function addOption(element, id, text) {
     var option = document.createElement("option");
     option.text = text;
     option.value = id;
@@ -37,7 +75,7 @@ $times = array(8, 9, 10, 11, 13, 14, 15, 16, 17);
       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         var result = JSON.parse(xmlhttp.response);
         addOption(doctorElement, 0, 'Selecione');
-        result.map(function(item){
+        result.map(function (item) {
           addOption(doctorElement, item.id, item.name);
         });
 
@@ -62,11 +100,15 @@ $times = array(8, 9, 10, 11, 13, 14, 15, 16, 17);
     xmlhttp.onreadystatechange = function () {
       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         var result = JSON.parse(xmlhttp.response);
-        result.map(function(item){
-          times = times.filter(function (value) { return value != item });
+        result.map(function (item) {
+          times = times.filter(function (value) {
+            return value != item
+          });
         });
         addOption(timeElement, 0, 'Selecione');
-        times.map(function (item) { addOption(timeElement, item, item) });
+        times.map(function (item) {
+          addOption(timeElement, item, item)
+        });
       }
     }
     xmlhttp.open("GET", "search_schedule.php?doctor=" + doctor + "&date=" + date, true);
@@ -85,7 +127,7 @@ $times = array(8, 9, 10, 11, 13, 14, 15, 16, 17);
     <div class="schedule-title">
       <h1>Agende sua consulta</h1>
     </div>
-    <form name="schedule-form" method="post" action="schedule_send.php">
+    <form name="schedule-form" method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
       <div class="row">
         <div class="col col-sm-12 col-xs-12 col-lg-12">
           <label for="medical-type">Especiliadade Médica</label>
@@ -111,12 +153,12 @@ $times = array(8, 9, 10, 11, 13, 14, 15, 16, 17);
           </select>
         </div>
         <div class="col col-sm-12 col-xs-12 col-lg-12">
-          <label for="pacient">Paciente</label>
-          <input type="text" name="pacient"/>
+          <label for="patient">Paciente</label>
+          <input type="text" name="patient"/>
         </div>
         <div class="col col-sm-12 col-xs-12 col-lg-12">
-          <label for="phone-pacient">Telefone (Paciente)</label>
-          <input type="text" name="phone-pacient"/>
+          <label for="patient-phone">Telefone (Paciente)</label>
+          <input type="text" name="patient-phone"/>
         </div>
         <div class="col col-sm-12 col-xs-12 col-lg-12">
           <button type="submit" name="submit">Agendar</button>
